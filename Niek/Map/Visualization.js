01a4bd6c9957077f43bdf5737a7810d6a856acb9
NiekMeffert <71270108+NiekMeffert@users.noreply.github.com>
2023-12-06 09:52:58 +0100

    Buublemap

--- Niek/Map/Visualization.js
+++ Niek/Map/Visualization.js
@@ -1,34 +1,30 @@
 (function() {
-    var margin = { top:100, left:100, right:100, bottom:100},
-    height = 1000 - margin.top - margin.bottom,
-    width = 2000 - margin.left - margin.right;
+    var margin = { top: 100, left: 100, right: 100, bottom: 100 },
+        height = 1000 - margin.top - margin.bottom,
+        width = 2000 - margin.left - margin.right;
 
     var svg = d3.select("#map")
-    .append("svg")
-    .attr("height", height + margin.top + margin.bottom)
-    .attr("width", width + margin.left + margin.right)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        .append("svg")
+        .attr("height", height + margin.top + margin.bottom)
+        .attr("width", width + margin.left + margin.right)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     //Handles which datasets to use to depict info on the screen
-
     d3.queue()
-    .defer(d3.json, "world.topojson")
-    .defer(d3.csv, "capitalCities.csv")
-    .await(ready)
+        .defer(d3.json, "world.topojson")
+        .defer(d3.csv, "geo_no_camps.csv")
+        .await(ready)
 
     //Handles which projection format the map should be in.
-
     var projection = d3.geoMercator()
-    .translate ([ width / 2, height / 1])
-    .scale(500)
-
+        .translate([width / 2, height / 1])
+        .scale(500);
 
     var path = d3.geoPath()
-    .projection(projection)
+        .projection(projection);
 
-    function ready (error, data, capitals) 
-    {
+    function ready(error, data, entities) {
         console.log(data)
 
         var countries = topojson.feature(data, data.objects.countries).features
@@ -36,105 +32,72 @@
         console.log(countries)
 
         //Handles visual creation of the map via the json file.
-
         svg.selectAll(".country")
-        .data(countries)
-        .enter() .append("path")
-        .attr("class" , "country")
-        .attr("d" , path)
-        
+            .data(countries)
+            .enter().append("path")
+            .attr("class", "country")
+            .attr("d", path);
 
+        // Group entities by city name
+        var groupedEntities = d3.nest()
+            .key(function(d) { return d.properties_Place_name; })
+            .entries(entities);
 
-       
+        //Handles the CSV data for the cities and shows them as bubble map
+        var bubbles = svg.selectAll(".city-bubble")
+            .data(groupedEntities)
+            .enter().append("circle")
+            .attr("r", function(d) {
+                // Adjust the radius based on the number of entities in each city
+                return Math.sqrt(d.values.length) * 1;
+            })
+            .attr("class", "city-bubble")
+            .attr("cx", function(d) {
+                var coords = projection([d.values[0].geometry_coordinates_long, d.values[0].geometry_coordinates_lat]);
+                return coords[0];
+            })
+            .attr("cy", function(d) {
+                var coords = projection([d.values[0].geometry_coordinates_long, d.values[0].geometry_coordinates_lat]);
+                return coords[1];
+            })
+            .attr("fill-opacity", 0.6) // Set the fill-opacity to 0.6 for semi-transparency
+            .on('mouseover', function(d) {
+                d3.select(this).classed("selected", true);
 
+                // Show city name when hovering over the circle
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                tooltip.html(d.key)
+                    .style("left", (d3.event.pageX + 10) + "px") // Adjust left position
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on('mouseout', function(d) {
+                d3.select(this).classed("selected", false);
 
-        //Handles the CSV data for the countries and shows it as circles on the map
+                // Hide tooltip when moving out of the circle
+                tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
 
-        svg.selectAll(".city-circle")
-        .data(capitals)
-        .enter().append("circle")
-        .attr("r" , 4)
-        .attr("class" , "city-circle")
-        .attr("cx" , function(d)
-        {
-            var coords = projection ([d.capital_lng, d.capital_lat])
-            return coords[0];
-        })
-
-        .attr("cy" , function(d)
-        {
-            var coords = projection ([d.capital_lng, d.capital_lat])
-            return coords[1];
-        })
-
-        .on('mouseover' , function(d) 
-        {
-            d3.select(this).classed("selected" , true)
-        })
-
-        .on('mouseout' , function(d) 
-        {
-            d3.select(this).classed("selected" , false)
-        })
-
-     // test for more formats at ones
-
-        svg.selectAll(".city-circle")
-        .data(capitals)
-        .enter().append("circle")
-        .attr("r" , 4)
-        .attr("class" , "city-circle")
-        .attr("cx" , function(d)
-        {
-            var coords = projection ([d.lang, d.lat])
-            return coords[0];
-        })
-
-        .attr("cy" , function(d)
-        {
-            var coords = projection ([d.lang, d.lat])
-            return coords[1];
-        })
-
-        //Handles the CSV data for the country names and shows it as text on the map
-
-        svg.selectAll(".city-label")
-        .data(capitals)
-        .enter().append("text")
-        .attr("class" , "city-label")
-        .attr("x" , function(d)
-        {
-            var coords = projection ([d.capital_lng, d.capital_lat])
-            return coords[0];
-        })
-
-        .attr("y" , function(d)
-        {
-            var coords = projection ([d.capital_lng, d.capital_lat])
-            return coords[1];
-        })
-        .text (function(d)
-        {
-            return d.name
-        })
-
-        .attr("dx", 5)
-        .attr("dy", 2)
-
-
+        // Tooltip for displaying city names
+        var tooltip = d3.select("#map").append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 0);
     }
 
+
+    //Handles zoom function
     var zoom = d3.zoom()
-    .scaleExtent([1, 8])
-    .on("zoom", zoomed);
+        .scaleExtent([1, 30])
+        .on("zoom", zoomed);
 
-  svg.call(zoom);
+    svg.call(zoom);
 
-  function zoomed() {
-    svg.selectAll("path,circle")
-      .attr("transform", d3.event.transform);
-    };
-
-    
+    function zoomed() {
+        svg.selectAll("path, circle, text")
+            .attr("transform", d3.event.transform);
+    }
 
 })();
