dfaa1399e1d99596d39ab7d5b819ef5a78187226
johkir-creator <146092833+johkir-creator@users.noreply.github.com>
2023-12-06 09:31:39 +0100

    updates

--- data_cleaning/text_frequencies.py
+++ data_cleaning/text_frequencies.py
@@ -7,24 +7,47 @@ Created on Tue Dec  5 14:02:24 2023
 
 import os
 
-# set path
+
+
+
+def clean_text(text):
+    text = t.read()
+    text = text.replace(',', '')
+    text = text.replace('.', ' ')
+    text = text.replace(':', '')
+    text = text.replace(';', '')
+    text = text.lower()
+    word_list = text.split()
+    return word_list
+    
+
+def count_frequencies(wordlist):
+    frequencies = {}
+    for word in wordlist:
+        if word not in frequencies.keys():
+            frequencies[word] = 1
+        elif word in frequencies.keys(): 
+            frequencies[word] += 1
+    
+    return frequencies
+
+
+
+
+   # set path
 cwd = os.getcwd()
 path = cwd+'\\data\\text-en_clean\\'
-
+    
 # get list of documents in path
 l = os.listdir(path)
-
-
 
 for i in range(1):
     file = path+l[i]
     print(file)
     t = open(file, encoding='UTF-8')
-    text = t.read()
-    x = text.split()
+    words = clean_text(t)
+    f = count_frequencies(words)
 
-
-frequencies = {}
 
 
 '''
@@ -48,3 +71,33 @@ format for frequencies dict
   ] 
  }
 '''
+
+# Following an online guide
+import nltk
+from nltk import word_tokenize
+from nltk.probability import FreqDist
+import urllib.request
+from matplotlib import pyplot as plt
+nltk.download('punkt')
+
+text = t.read()
+
+# set path
+cwd = os.getcwd()
+path = cwd+'\\data\\text-en_clean\\'
+    
+# get list of documents in path
+l = os.listdir(path)
+
+for i in range(1):
+    file_path = path+l[i]
+    print(file_path)
+    text_file = open(file, encoding='UTF-8')
+    text = text_file.read()
+    print(text)
+
+#tokenize text by words
+words = word_tokenize(text)
+
+#check the number of words
+print(f"The total number of words in the text is {len(words)}")
